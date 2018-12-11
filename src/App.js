c73b2f2518218b90d020ea1e6a6f866df49c15a4
jalips <jservice@protonmail.com>
2018-12-11 11:03:40 +0100

    chap2 - 4 : Contextualisez le contenu de vos composants

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     }
 
     render() {
+        const won = new Date().getSeconds() % 2 === 0
         return (
             <div className="memory">
                 <GuessCount guesses={0} />
@@ -20,8 +21,7 @@ class App extends Component {
                 <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
                 <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
                 <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
-
-
+                {won && <p>GAGNÉ !</p>}
             </div>
         )
     }
