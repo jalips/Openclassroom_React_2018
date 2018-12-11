616fa7e45e1d1301c0374fa468b09da025a4b036
jalips <jservice@protonmail.com>
2018-12-11 16:21:38 +0100

    chap3 - 5 : Faites référence au bon « this » dans vos fonctions

--- src/App.js
+++ src/App.js
@@ -25,8 +25,9 @@ class App extends Component {
         return shuffle(result)
     }
 
-    handleCardClick(card) {
-        console.log(card, 'clicked')
+    // Arrow fx for binding
+    handleCardClick = (card) => {
+        console.log(card, this)
     }
 
     render() {
